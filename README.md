ed59d1d754cfb092a44c427d87f04ce1596f237b
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-10-08 08:47:08 +0000

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS CDK Study
+
+[![Open in Dev Containers](https://img.shields.io/static/v1?label=Dev%20Containers&message=Open&color=blue&logo=visualstudiocode)](https://vscode.dev/redirect?url=vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=https://github.com/kaito01234/aws-cdk-study)
---
+++ test/aws-cdk-study.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkStudy from '../lib/aws-cdk-study-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-study-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkStudy.AwsCdkStudyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
