42f07a6fbace938be338c1ef74f87c01d712dbe6
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-10-09 04:55:03 +0000

    add bastion

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,7 @@
 
 # set alias
 echo 'alias ll="ls -alF"' >> ~/.bash_aliases
+echo 'alias g="git"' >> ~/.bash_aliases
 echo 'alias d="docker"' >> ~/.bash_aliases
 echo 'alias dc="docker compose"' >> ~/.bash_aliases
 
